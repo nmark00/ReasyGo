99faadb4b13bce225fff33c1680f230eb54bc3bb
BrysonXiao <40129652+BrysonXiao@users.noreply.github.com>
2022-01-19 20:49:15 -0800

    Test commit

--- App.js
+++ App.js
@@ -1,42 +1,30 @@
 import * as React from 'react';
-import { View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {View, Text} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import auth from '@react-native-firebase/auth';
 
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
-
+import {createSwitchNavigator, createAppContainer} from 'react-navigation';
 
 import HomeNavigator from './src/navigators/HomeNavigator';
 import LandingNavigator from './src/navigators/LandingNavigator';
 
-
 const Stack = createStackNavigator();
-
 
 export default function App() {
   let initRoute;
   if (auth().currentUser && auth().currentUser.displayName)
-    initRoute = "HomeNavigator";
-  else initRoute = "LandingNavigator";
+    initRoute = 'HomeNavigator';
+  else initRoute = 'LandingNavigator';
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName={initRoute}
-          screenOptions={{headerShown: false}}
-        >
-          <Stack.Screen
-            name="LandingNavigator"
-            component={LandingNavigator}
-          />
-          <Stack.Screen
-            name="HomeNavigator"
-            component={HomeNavigator}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    );
-
-  
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={initRoute}
+        screenOptions={{headerShown: false}}>
+        <Stack.Screen name="LandingNavigator" component={LandingNavigator} />
+        <Stack.Screen name="HomeNavigator" component={HomeNavigator} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
